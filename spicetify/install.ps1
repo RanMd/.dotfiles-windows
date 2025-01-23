dba6ce692734d6ef5c1bfba984a9f14da84c7fdd
RanMd <randommid1873@gmail.com>
2025-01-22 22:52:44 -0500

    Add no controls extension

--- spicetify/install.ps1
+++ spicetify/install.ps1
@@ -1,4 +1,4 @@
-$spotifyPath = "$env:USERPROFILE\AppData\Roaming\Spotify\Spotify.exe"
+$spotifyPath = "$env:APPDATA\Spotify\Spotify.exe"
 
 function fail {
     param($message)
@@ -11,12 +11,37 @@ function fail {
     exit
 }
 
+function success {
+    param($message)
+    Write-Host "`r`e[2K  [ " -NoNewline
+    [Console]::ForegroundColor = 'Green'
+    Write-Host 'OK' -NoNewline
+    [Console]::ResetColor()
+    Write-Host " ] $message"
+}
+
+
 if (-not (Test-Path -Path $spotifyPath)) {
   fail 'Debes instalar Spotify primero.'
   return  
 }
 
+# Install Spicetify CLI
 iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex
+
+# Themes
 git clone --depth=1 https://github.com/spicetify/spicetify-themes.git
+
+if (-not (Test-Path -Path "./spicetify-themes")) {
+    fail 'Hubo un problema al clonar el repositorio de temas.'
+}
+
 cd spicetify-themes
 cp * "$(spicetify -c | Split-Path)\Themes\" -Recurse
+cd ..
+
+# Hide windows controls
+$extensionPath = (Resolve-Path "$env:APPDATA\spicetify\Extensions\noControls.js").Path 
+curl -o $extensionPath https://raw.githubusercontent.com/ohitstom/spicetify-extensions/refs/heads/main/noControls/noControls.js
+
+success 'Todo se ha instalado correctamente, no te olvides de ejecutar "spicetify apply" para aplicar los cambios'
